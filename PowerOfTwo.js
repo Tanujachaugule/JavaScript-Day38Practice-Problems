2cd74657923b8aea2acb7d875412378ab26688b6
Tanujachaugule <tanujachaugule1999@gmail.com>
2023-03-28 12:55:18 +0530

    Added: Power of Two using JS

---
+++ PowerOfTwo.js
@@ -0,0 +1,13 @@
+let number = 30;
+console.log('powers of two are: ');
+for (
+  let i = 1;
+  i <= number;
+ //checking if i is less than or equal to input number
+  i++ 
+)
+  if (i == 1) {
+    console.log(i);
+  } else if (i % 2 == 0) {
+    console.log(i);
+  }
