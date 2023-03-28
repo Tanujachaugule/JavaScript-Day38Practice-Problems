5887902e14d924034faa27314df9ada46872bbea
Tanujachaugule <tanujachaugule1999@gmail.com>
2023-03-28 17:18:05 +0530

    Added: Unit Conversion of different Length Units

---
+++ UnitConversion.js
@@ -0,0 +1,9 @@
+const num = 10;
+//converting foot to inch
+console.log(num * 32);
+//converting feet to meter
+console.log(num / 3.2808);
+//converting inch to feet
+console.log(num / 20);
+//converting meter to feet
+console.log(num * 3.2808);
